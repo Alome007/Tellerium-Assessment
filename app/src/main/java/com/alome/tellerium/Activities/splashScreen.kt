6b6a10162549eb71edf873d02999028831a7ce0f
alome007 <astrocoder007@gmail.com>
2020-10-23 13:25:08 +0100

    Refactored codebase into packages and included libraries

---
+++ app/src/main/java/com/alome/tellerium/Activities/splashScreen.kt
@@ -0,0 +1,2 @@
+package com.alome.tellerium.Activities
+
--- app/src/main/java/com/alome/tellerium/Utils/constants.java
+++ app/src/main/java/com/alome/tellerium/Utils/constants.java
@@ -4,5 +4,4 @@ public class constants {
     static int MAX_ITEM=10;
     public static String IMAGE_BASE_URL="https://s3-eu-west-1.amazonaws.com/agromall-storage/";
     public static String BASE_URL="https://theagromall.com/api/v2/get-sample-farmers?limit="+MAX_ITEM;
-
 }
